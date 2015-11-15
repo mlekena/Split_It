dbe3420c9e232dfa7d709846969865717ae919d5
Modupe Theko Lekena <mlekena@skidmore.edu>
2015-11-14 19:26:38 -0500

    rename index.html to index.es and create views folder and move asset folder and index.ejs in it

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var app = express()
 
 //define our render engine. It will allow use to embed Javascript in our HTML page. file extention is .ets
 app.set('view engine', 'ejs');
-
-app.get('/', function(request,response){
+app.use(express.static('views'));
+app.get('/splitit', function(request,response){
 	response.render('index');
 });
 
 //SHOULD BE THE LAST ROUTE IN FILE
 
-app.get('*', function(request,response){
+/*app.get('*', function(request,response){
 	console.log("incorrect address passed")
 	response.render('Bad route. Go to http://localhost:3000/');
-});
+});*/
 
 var server = app.listen(3000, function(){
-	console.log("Listening... go to http://localhost:3000/")
+	console.log("Listening... go to http://localhost:3000/splitit")
 });
